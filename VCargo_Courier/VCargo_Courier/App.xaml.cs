a7e729919b9db9ac0cf82c9d10ece58b8833de5c
63927 <63927@DESKTOP-F1FSBP5>
2021-11-03 13:59:37 +0800

    Add project files.

---
+++ VCargo_Courier/VCargo_Courier/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using VCargo_Courier.Services;
+using VCargo_Courier.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VCargo_Courier
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<UserMockDataStore>();
+            DependencyService.Register<MockDataStore>();
+            MainPage = new LoginPage();
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
